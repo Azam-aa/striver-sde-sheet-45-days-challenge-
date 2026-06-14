213c3ee92cd9da36d3ac0d3f95078e48c7b7ef66
Azam-aa <azamp442@gmail.com>
2026-06-14 13:37:21 +0530

    Day 14 - Striver SDE Sheet Challenge

---
+++ day-14/trapping-rain-water/TrappingRainWater.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class TrappingRainWater {
+
+    public static int trap(
+            int[] height){
+
+        int left = 0;
+        int right =
+            height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while(left <= right){
+
+            if(height[left]
+                <= height[right]){
+
+                if(height[left]
+                    >= leftMax){
+
+                    leftMax =
+                        height[left];
+
+                }else{
+
+                    water +=
+                        leftMax -
+                        height[left];
+                }
+
+                left++;
+
+            }else{
+
+                if(height[right]
+                    >= rightMax){
+
+                    rightMax =
+                        height[right];
+
+                }else{
+
+                    water +=
+                        rightMax -
+                        height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr =
+            new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(
+            trap(arr)
+        );
+
+        sc.close();
+    }
+}
